4f03966092bdc9f4ecab239b976c0c43621132d8
ltifimedamin <115627037+ltifimedamin@users.noreply.github.com>
2023-11-18 17:12:02 +0100

    real madrid

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -10,6 +10,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
+
 
 #[Route('/avis')]
 class AvisController extends AbstractController
@@ -21,7 +29,70 @@ class AvisController extends AbstractController
             'avis' => $avisRepository->findAll(),
         ]);
     }
-
+    #[Route('/download/excel/avis', name: 'download_excel_avis')]
+    public function downloadExcelFromAvis(EntityManagerInterface $entityManager, FlashBagInterface $flashBag): BinaryFileResponse
+    {
+        // Récupérer les données de la table "avis" depuis la base de données
+        $avisRepository = $entityManager->getRepository(Avis::class);
+        $avisData = $avisRepository->findAll(); // Vous pouvez adapter cette requête selon vos besoins
+            
+        // Définir le chemin du répertoire où le fichier sera enregistré
+        $directoryPath = 'C:\Users\Ltifi\Downloads\last vers\ihebiheb\Piweb-ihebevent\Piweb';
+    
+        // Vérifier si le répertoire existe, sinon le créer
+        if (!is_dir($directoryPath)) {
+            mkdir($directoryPath, 0777, true); // Créer le répertoire avec les permissions nécessaires
+        }
+    
+        // Définir le chemin complet du fichier Excel
+        $filePath = $directoryPath . '\fichier_excel_avis.xlsx';
+    
+        // Créer un écrivain (writer) pour le fichier Excel
+        $writer = WriterEntityFactory::createXLSXWriter();
+    
+        // Vérifier si le fichier existe déjà
+        if (!file_exists($filePath)) {
+            // Créer un tableau avec les titres des colonnes
+            $columnTitles = [
+                ['ID', 'Username', 'Titre avis', 'Pub avis', 'Date avis', 'Pub avis', 'Nom restaurant'],
+            ];
+        
+            // Ouvrir le fichier uniquement s'il n'existe pas déjà
+            $writer->openToFile($filePath);
+            
+            foreach ($columnTitles as $columnRow) {
+                // Créer une ligne avec les titres de colonnes
+                $titleRow = WriterEntityFactory::createRowFromArray($columnRow);
+                $writer->addRow($titleRow); // Ajouter la ligne des titres de colonnes
+               
+            }
+            foreach ($avisData as $avis) {
+                // Adapter ces lignes en fonction des propriétés de votre entité Avis
+                $row = [
+                    $avis->getId(),
+                    $avis->getUser()->getUsername(),
+                    $avis->getTitreavis(),
+                    $avis->getPubavis(),
+                    $avis->getDateavis()->format('Y-m-d'),
+                    $avis->getPubavis(), // Vérifiez si c'est la bonne propriété
+                    $avis->getRestaurant()->getNom(),
+                ];
+        
+                $rowEntity = WriterEntityFactory::createRowFromArray($row);
+                $writer->addRow($rowEntity);
+            }
+        
+            // Fermer le fichier Excel
+            $writer->close();
+        }
+        if (file_exists($filePath)) {
+            $flashBag->add('success', 'Le fichier a été téléchargé avec succès.');
+        }
+    
+        return $this->file($filePath, 'fichier_excel_avis.xlsx');
+        
+    }
+   
     #[Route('/new', name: 'app_avis_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
