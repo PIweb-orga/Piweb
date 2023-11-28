5b1bec5d2c9d0bb0b88a4ed41d35adca836ec55b
ihebiss <115627318+ihebiss@users.noreply.github.com>
2023-11-28 14:28:00 +0100

    commiiit 2

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -13,14 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Endroid\QrCode\QrCode;
-use Dompdf\Dompdf;
-
-use Dompdf\Options;
-
-
-
-
 #[Route('/participant')]
 class ParticipantController extends AbstractController
 {
@@ -79,54 +71,25 @@ public function new22(Request $request, EntityManagerInterface $entityManager): 
 public function new222(Request $request, EntityManagerInterface $entityManager, EvennementRepository $eventRepository,int $idevent): Response
 {
     $participant = new Participant();
-
+    
+    $form = $this->createForm(ParticipantType::class, $participant);
+    $form->handleRequest($request);
+   
     // Fetch the events from the repository
     $eventRepository = $entityManager->getRepository(Evennement::class);
     $evennements = $eventRepository->findAll();
-    $Event = $eventRepository->findOneBy(['idevent' => $idevent]);
-
-    // Check if the event is not null before accessing its properties
-    if ($Event) {
-        // Set default values for the form fields
-        $participant->setEvent($Event);
-
-        // Create the form with the participant data
-        $form = $this->createForm(ParticipantType::class, $participant);
-    } else {
-        // Handle the case where the event is not found
-        // You may want to throw an exception or handle it according to your needs
-        // For now, creating an empty form
-        $form = $this->createForm(ParticipantType::class, $participant);
-    }
-
-    $form->handleRequest($request);
-
+    $Event=$eventRepository->find($idevent);
     if ($form->isSubmitted() && $form->isValid()) {
-        // Handle form submission
         $evennement = $participant->getEvent();
-    
-        // Ensure that the participant ID is a valid string
-        $participantId = (string) $participant->getIdparticipant();
-    
-        // Generate QR Code
-        $qrCode = new QrCode($participantId);
-        $qrCode->setSize(300);
-    
-        // Save the QR Code as an image (you can adjust the path)
-        $qrCode->writeFile('path/to/qrcodes/qrcode_' . $participantId . '.png');
-    
         $entityManager->persist($participant);
         $entityManager->flush();
-    
-        return $this->redirectToRoute('app_participant_index');
-    
     }
 
-    return $this->render('participant/newfront.html.twig', [
+    return $this->renderForm('participant/newfront.html.twig', [
         'participant' => $participant,
-        'form' => $form->createView(),
+        'form' => $form,
         'Event' => $Event,
-        'evennement' => $evennements,
+        'evennement' => $evennements, // Pass the events to the template
     ]);
 }
 
@@ -166,33 +129,5 @@ public function new222(Request $request, EntityManagerInterface $entityManager, 
         }
 
         return $this->redirectToRoute('app_participant_index', [], Response::HTTP_SEE_OTHER);
-    }
-    #[Route('/pdf', name: 'pdf')]
-    public function pdf(Participant $participant,ParticipantRepository $ParticipantRepository)
-    {
-        // Configure Dompdf according to your needs
-        $pdfOptions = new Options();
-        $pdfOptions->set('defaultFont', 'Arial');
-
-        // Instantiate Dompdf with our options
-        $dompdf = new Dompdf($pdfOptions);
-        // Retrieve the HTML generated in our twig file
-        $html = $this->renderView('participant/pdf.html.twig', [
-            'participant' => $ParticipantRepository->findAll(),
-        ]);
-
-        // Load HTML to Dompdf
-        $dompdf->loadHtml($html);
-
-        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
-        $dompdf->setPaper('A4', 'portrait');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-        // Output the generated PDF to Browser (inline view)
-        $dompdf->stream("mypdf.pdf", [
-            "Attachment" => false
-        ]);
     }
 }
